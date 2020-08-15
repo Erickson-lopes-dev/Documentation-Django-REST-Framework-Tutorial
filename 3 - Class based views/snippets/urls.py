76260345d15be998b639820c3b8434fc97c8f81c
EricksonLopes <ofc.erickson@gmail.com>
2020-08-15 16:16:02 -0300

    Class based views

--- 3 - Class based views/snippets/urls.py
+++ 3 - Class based views/snippets/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippentDetail.as_view()),
+
+    path('snippets/mixins/', views.SnippetMixinsList.as_view()),
+    path('snippets/mixins/<int:pk>/', views.SnippentMixinsDetail.as_view()),
+
+    path('snippets/generic/', views.SnippetGenericsList.as_view()),
+    path('snippets/generic/<int:pk>/', views.SnippentGenericsDetail.as_view()),
 ]
--- 3 - Class based views/snippets/views.py
+++ 3 - Class based views/snippets/views.py
@@ -1,80 +1,128 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
+from django.http import Http404
+from rest_framework import status, mixins, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-@api_view(['GET', 'POST'])
-def snippet_list(request):
-    """
-        lista todos ou adiciona
-    """
-    # se a request for get
-    if request.method == 'GET':
-        # pega todos os objetos do banco de dados referente aquela tabela
+# View Baseada em classes
+class SnippetList(APIView):
+    # se o método for get
+    def get(self, request):
+        # pega todos os itens dentro da tabela
         snippets = Snippet.objects.all()
-        # serializa os dados para ser viasualizado em json
+        # serializa transformando em json
         serializer = SnippetSerializer(snippets, many=True)
-        # retorna os dados na request
-        return JsonResponse(serializer.data, safe=False)
+        # retorna os itens coletados
+        return Response(serializer.data)
 
-    # caso seja post
-    elif request.method == 'POST':
-        # Encapsula os dados no JSONParser
-        data = JSONParser().parse(request)
-        # passa pelo serializador para o python entender os dados
-        serializer = SnippetSerializer(data=data)
-        # verifica se os dados estão validos
-        if serializer.data.is_valid():
-            # caso sim, salva
+    def post(self, request):
+        # serializa os itens recebidos para o python ler
+        serializer = SnippetSerializer(data=request.data)
+        # verifica se o formulario é valido
+        if serializer.is_valid():
+            # se for valido salva
             serializer.save()
-            # retorna os dados e sucesso
-            return JsonResponse(serializer.data, status=201)
-        # caso nao seja válido retorna erro e status 400
-        return JsonResponse(serializer.errors, status=400)
+            # Retorna os dados e o status de create
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # se os dados nao forem válidos retorna erro no status
+        return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST', 'DELETE'])
-def snippet_detail(request, pk):
-    """
-    Retrive, Update, delete
-    :param request:
-    :param pk:
-    :return:
-    """
-    try:
-        # primeiro o sistema tenta encontrar o pk dentro do banco de dados
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        # retorna um erro de nao encontrado
-        return HttpResponse(status=404)
+# View Baseada em classes
+class SnippentDetail(APIView):
+    # função que procura o objeto dentro da base de dados
+    def get_object(self, pk):
+        try:
+            # procura o pk do item e o retorna
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            # caso não encontre, retorna um erro 404
+            raise Http404
 
     # se o método for get
-    if request.method == 'GET':
-        # serializa o snippet encontrado
+    def get(self, request, pk):
+        # executa a função introduzindo dentro da variavel o valor recebido
+        snippet = self.get_object(pk)
+        # serializa para json o item encontrado
         serializer = SnippetSerializer(snippet)
-        # retorna o snippet encontrado
-        return JsonResponse(serializer.data)
+        # retorna o item encontrado como json
+        return Response(serializer.data)
 
-    # se o método for PUT(alterar)
-    elif request.method == "POST":
-        # recebe os dados enviado na request
-        data = JSONParser().parse(request)
-        # verifica o snippet encontrado e os dados recebido
-        serializer = SnippetSerializer(snippet, data=data)
-        # verifica se é valido o formulário
+    # se o método for put
+    def put(self, request, pk):
+        # executa a função introduzindo dentro da variavel o valor recebido
+        snippet = self.get_object(pk)
+        # verifica serializa para dentro da base de dados
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
-            # salva as alterações
+            # se for valido salva
             serializer.save()
-            # retorna os dados
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            # Retorna os dados e o status de create
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # se os dados nao forem válidos retorna erro no status
+        return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
 
-    # se for delete(deletar)
-    elif request.method == "DELETE":
-        # deleta do banco de dados
+    def delete(self, request, pk):
+        # verifica se o objeto existe
+        snippet = self.get_object(pk)
+        # deleta o objeto
         snippet.delete()
-        # retorna uma resposta que foi sucesso
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# ----------------------------------------------------
+
+# View Baseada em classes com mixins
+# importe os mixins que irá utilizar
+class SnippetMixinsList(mixins.ListModelMixin,
+                        mixins.CreateModelMixin,
+                        generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    # para realizar alterações
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+# View Baseada em classes com mixins
+# importe os mixins que irá utilizar
+class SnippentMixinsDetail(mixins.RetrieveModelMixin,
+                           mixins.UpdateModelMixin,
+                           mixins.DestroyModelMixin,
+                           generics.GenericAPIView):
+    # passar para classe a base de dados que deseja
+    queryset = Snippet.objects.all()
+    # o serializador
+    serializer_class = SnippetSerializer
+
+    # caso deseje alterar alguma coisa
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+# -----------------------------------------------------
+
+# View Baseada em classes com generics
+# importe os generics que irá utilizar
+class SnippetGenericsList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+# View Baseada em classes com generics
+# importe os generics que irá utilizar
+class SnippetGenericsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
