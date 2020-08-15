6533a4973927017edbd60f5a7cc48669e2d83ad1
EricksonLopes <ofc.erickson@gmail.com>
2020-08-15 12:18:18 -0300

    quickstart

---
+++ 0 - Quickstart/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework
+
+class AlbumSerializer(serializers.ModelSerializer):
+    tracks = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Album
+        fields = ['album_name', 'artist', 'tracks']
